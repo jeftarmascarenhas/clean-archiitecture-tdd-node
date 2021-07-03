c837e0235c91a3392ecc83691eece5718b7ea306
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-07-03 02:24:16 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.expors = {
+  roots: ["<rootDir>/src"],
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+}
